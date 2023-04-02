1e9f931c7f0a89b2fd721dfd63a16c84c2cc69ac
Rebeca Victoria Lucas <rebecavictoria21@gmail.com>
2023-04-02 13:14:54 +0200

    Limpieza general

--- tabla_definitiva.py
+++ tabla_definitiva.py
@@ -1,18 +1,31 @@
 #path =  "./20220305-DatosNormalizados-AsignaturaElectronicaCircuitos-Curso2021-GrupoE.xlsx"
 tabla = "GrupoE"
 #tabla = "GrupoA"
-url = 'mysql+pymysql://excel_user:patata@127.0.0.1/EC' #credenciales(excel_user), contraseña(patata) y host(localhost)
+url = 'mysql+pymysql://root:patata@127.0.0.1/EC' #credenciales(excel_user), contraseña(patata) y host(localhost)
 
 import os
 import sys
 import pandas as pd
 from sqlalchemy import create_engine 
 from sqlalchemy_utils import database_exists, create_database
-from flask import Flask, render_template
+ 
+def selfichero():
+    ficheros_dir = './'
+    with os.scandir(ficheros_dir) as ficheros:
+        ficheros = [fichero.name for fichero in ficheros if fichero.is_file() and fichero.name.endswith('.xlsx')]
+    print("Estos son los ficheros que hay en esta carpeta:")
+    for n in ficheros:
+        print(f"[{ficheros.index(n)}]" + n)     
+    excel_seleccionado=ficheros[int(input("-Selecciona el fichero que quieres analizar-->"))]
+    print("./" + excel_seleccionado)
+    return "./" + excel_seleccionado
 
-def excel2mysql(file):
-    path = file
+
+def excel2mysql():
+    path = selfichero()
     x1 = pd.read_excel(path, sheet_name = "Datos", usecols="S,X:AY,BL:BN", skiprows=11, nrows=12, header=0, index_col=None)
+    #ID = pd.read_excel(path, sheet_name = "Datos", usecols="S", skiprows=11, header=0, index_col=0)
+
 
     x1 = x1.fillna(0)
     
@@ -24,7 +37,9 @@ def excel2mysql(file):
     with engine.connect() as conn, conn.begin():
         x1.to_sql(tabla, conn, if_exists='replace')
     print ("Migración terminada!")
-    return render_template('finish.html')
+ 
  
 if __name__ == '__main__':
     excel2mysql()
+
+#  https://www.manejandodatos.es/2019/04/migracion-de-excel-a-mysql/
